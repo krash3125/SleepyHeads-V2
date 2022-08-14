a01f8c2f30e81817fc8e53aefaa54dac08700f00
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2022-08-14 02:24:31 -0700

    sounds page

--- components/BottomSheets/Sounds.jsx
+++ components/BottomSheets/Sounds.jsx
@@ -1,11 +1,13 @@
-import React, { useMemo } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useMemo } from 'react';
+import { View, Text } from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
 import tw from '../../lib/tailwind';
 
 const Sounds = ({ soundsBottomSheetRef }) => {
   // variables
   const snapPoints = useMemo(() => ['92%'], []);
+  const [sound, setSound] = useState(0);
 
   return (
     <BottomSheet
@@ -26,21 +28,36 @@ const Sounds = ({ soundsBottomSheetRef }) => {
         <Text style={tw`text-3.5xl font-800 text-sh-dark-blue text-center`}>
           Sounds
         </Text>
-        <View
-          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-[#E65C4FCC] rounded-lg items-center px-4 mt-4`}
-        >
-          <Text style={tw`text-xl font-600 text-sh-dark-blue `}>Sound 1</Text>
-        </View>
-        <View
-          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-[#E65C4FCC] rounded-lg items-center px-4 mt-4`}
-        >
-          <Text style={tw`text-xl font-600 text-sh-dark-blue `}>Sound 1</Text>
-        </View>
-        <View
-          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-[#E65C4FCC] rounded-lg items-center px-4 mt-4`}
-        >
-          <Text style={tw`text-xl font-600 text-sh-dark-blue `}>Sound 1</Text>
-        </View>
+        <BouncyCheckbox
+          isChecked={sound === 0}
+          disableBuiltInState={true}
+          text="Sound 1"
+          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-sh-dark-blue rounded-lg items-center px-4 mt-4`}
+          textStyle={tw`text-xl font-600 text-white no-underline tracking-widest`}
+          innerIconStyle={tw`border border-[#E98F50]`}
+          fillColor="#E98F50"
+          onPress={() => setSound(0)}
+        />
+        <BouncyCheckbox
+          isChecked={sound === 1}
+          disableBuiltInState={true}
+          text="Sound 2"
+          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-sh-dark-blue rounded-lg items-center px-4 mt-4`}
+          textStyle={tw`text-xl font-600 text-white no-underline tracking-widest`}
+          innerIconStyle={tw`border border-[#E98F50]`}
+          fillColor="#E98F50"
+          onPress={() => setSound(1)}
+        />
+        <BouncyCheckbox
+          isChecked={sound === 2}
+          disableBuiltInState={true}
+          text="Sound 3"
+          style={tw`flex flex-row h-14 w-full border border-zinc-800 bg-sh-dark-blue rounded-lg items-center px-4 mt-4`}
+          textStyle={tw`text-xl font-600 text-white no-underline tracking-widest`}
+          innerIconStyle={tw`border border-[#E98F50]`}
+          fillColor="#E98F50"
+          onPress={() => setSound(2)}
+        />
       </View>
     </BottomSheet>
   );
