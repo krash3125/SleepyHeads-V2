3befe8d04a078a7244ebc96ff45b567f095294ba
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2022-10-02 22:52:00 -0700

    title change

--- screens/SplashScreen.jsx
+++ screens/SplashScreen.jsx
@@ -19,9 +19,9 @@ const SplashScreen = ({ navigation }) => {
       <View style={tw`flex-1`}>
         <Image source={fullLogo} style={tw`h-1/2 w-full mx-auto`} />
         <Text style={tw`my-4 mx-3 text-4xl font-800 text-center`}>
-          The perfect app for long drives!
+          The perfect app for late nights!
         </Text>
-        <Text style={tw`text-lg text-center`}>Driving safely made easy</Text>
+        <Text style={tw`text-lg text-center`}>Staying awake made easy</Text>
         <TouchableOpacity
           style={[
             tw`w-3/4 mt-16 mx-auto p-3 rounded-md`,
